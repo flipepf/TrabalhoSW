ef73d5b7407fbce82e3bab0b53c252a5d09fa833
flipepf <flipepf@gmail.com>
2019-10-08 22:29:21 -0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -8,37 +8,57 @@ var dao = new ProdutoDao();
 
 /* GET */
 router.get('/produtos', function(req, res) {
+   res.status(200)
    res.send(dao.getAll())
 })
 
 router.get('/produtos/:id', function(req, res) {
    var id = req.params.id
-   res.send(dao.getById(parseInt(id)))
+   resposta = dao.getById(parseInt(id))
+   if (resposta!=null) {
+      res.status(200)
+      res.send(JSON.stringify(resposta))
+   } else {
+      res.status(404)
+      res.send("Produto não localizado")
+   }
 })
 
 /* POST */
 router.post('/produtos', function(req, res) {
    if (req.body.constructor === Object && Object.keys(req.body).length === 0){
-      res.send("Objeto vazio")
+      res.status(400)
+      res.send("Parametro vazio")
    } else {
       var envio = req.body //PEGA O CONTEUDO PASSADO NA REQUISIÇÃO
       var Prod = new Produto(envio.id, envio.nome, envio.descricao, envio.preco, envio.categoria) //CRIA O OBJETO COM O CONTEÚDO PASSADO NA REQUISIÇÃO
       var resposta = dao.add(Prod) //ARMAZENA O OBJETO NO ARRAY
-      if (resposta!=null) res.send(JSON.stringify(resposta))
-      else res.send("ID "+envio.id+" já cadastrado")
+      if (resposta!=null){
+         res.status(201)
+         res.send("Produto cadastrado com sucesso!")
+      } else {
+         res.status(409)
+         res.send("ID "+envio.id+" já cadastrado")
+      }
    }
 })
 
 /* PUT */
 router.put('/produtos/:id', function(req, res) {
    if (req.body.constructor === Object && Object.keys(req.body).length === 0){
-      res.send("Objeto vazio")
+      res.status(400)
+      res.send("Parametro vazio")
    } else { 
-      var obj = req.body //PEGA O CONTEUDO PASSADO NA REQUISIÇÃO
-      var id = req.params.id //PEGA O ID PASSADO POR PARAMETRO
-      var resposta = dao.update(obj, parseInt(id)) //REALIZA O UPDATE
-      if (resposta!=null) res.send(JSON.stringify(resposta))
-      else res.send("Produto não localizado")
+      var obj = req.body 
+      var id = req.params.id 
+      var resposta = dao.update(obj, parseInt(id)) 
+      if (resposta!=null){
+         res.status(200)
+         res.send("Produto de ID: "+id+" alterado com sucesso!")
+      } else {
+         res.status(404)
+         res.send("Produto não localizado")
+      }
    }
 })
 
@@ -46,8 +66,14 @@ router.put('/produtos/:id', function(req, res) {
 router.delete('/produtos/:id', function(req, res) {
    var id = req.params.id
    var index = dao.remove(parseInt(id));
-   if (index!=null) res.send("Produto de ID: "+id+" removido!")
-   else res.send("Produto não localizado")
+   if (index!=null) {
+      res.status(200)
+      res.send("Produto de ID: "+id+" removido!")
+   }
+   else {
+      res.status(404)
+      res.send("Produto não localizado")
+   }
 })
 
 module.exports = router
