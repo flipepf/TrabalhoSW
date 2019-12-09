dacb5fdb0e71313d40552080ccab4754a9329499
flipepf <flipepf@gmail.com>
2019-12-09 18:10:46 -0300

    versao 3.1
    POST, PUT e DELETE somente após login

--- routes/api.js
+++ routes/api.js
@@ -9,78 +9,80 @@ const ProdutoDao = require('../Model/ProdutoDAO')
 const auth = require('../middlewares/authenctication')
 
 var dao = new ProdutoDao();
+var isLogin = false
 
 /* GET */
 router.get('/produtos', function(req, res) {
-   res.status(200)
-   res.send(dao.getAll())
+   res.status(200).send(dao.getAll())
 })
 
 router.get('/produtos/:id', function(req, res) {
    var id = req.params.id
    resposta = dao.getById(parseInt(id))
    if (resposta!=null) {
-      res.status(200)
-      res.send(JSON.stringify(resposta))
+      res.status(200).send({ status: 200, message: JSON.stringify(resposta)})
    } else {
-      res.status(404)
-      res.send("Produto não localizado")
+      res.status(404).send({ status: 404, message: "Produto não localizado",
+     })
    }
 })
 
 /* POST */
 router.post('/produtos', function(req, res) {
-   if (req.body.constructor === Object && Object.keys(req.body).length === 0){
-      res.status(400)
-      res.send("Parametro vazio")
+   if (!isLogin){
+      res.status(200).send({ status: 200, message: "Necessário efetuar login para acessar este recurso!"})
    } else {
-      var envio = req.body //PEGA O CONTEUDO PASSADO NA REQUISIÇÃO
-      var Prod = new Produto(envio.id, envio.nome, envio.descricao, envio.preco, envio.categoria) //CRIA O OBJETO COM O CONTEÚDO PASSADO NA REQUISIÇÃO
-      var resposta = dao.add(Prod) //ARMAZENA O OBJETO NO ARRAY
-      if (resposta!=null){
-         res.status(201)
-         res.send("Produto cadastrado com sucesso!")
+      if (req.body.constructor === Object && Object.keys(req.body).length === 0){
+         res.status(400).send({ status: 400, message: "Parametro vazio" })
       } else {
-         res.status(409)
-         res.send("ID "+envio.id+" já cadastrado")
+         var envio = req.body //PEGA O CONTEUDO PASSADO NA REQUISIÇÃO
+         var Prod = new Produto(envio.id, envio.nome, envio.descricao, envio.preco, envio.categoria) //CRIA O OBJETO COM O CONTEÚDO PASSADO NA REQUISIÇÃO
+         var resposta = dao.add(Prod) //ARMAZENA O OBJETO NO ARRAY
+         if (resposta!=null){
+            res.status(201).send({ status: 201, message: "Produto cadastrado com sucesso!" })
+         } else {
+            res.status(409).send({ status: 400, message: "ID "+envio.id+" já cadastrado" })
+         }
       }
    }
 })
 
 /* PUT */
 router.put('/produtos/:id', function(req, res) {
-   if (req.body.constructor === Object && Object.keys(req.body).length === 0){
-      res.status(400)
-      res.send("Parametro vazio")
-   } else { 
-      var obj = req.body 
-      var id = req.params.id 
-      var resposta = dao.update(obj, parseInt(id)) 
-      if (resposta!=null){
-         res.status(200)
-         res.send("Produto de ID: "+id+" alterado com sucesso!")
-      } else {
-         res.status(404)
-         res.send("Produto não localizado")
+   if (!isLogin){
+      res.status(200).send({ status: 200, message: "Necessário efetuar login para acessar este recurso!"})
+   } else {      
+      if (req.body.constructor === Object && Object.keys(req.body).length === 0){
+         res.status(400).send({ status: 400, message: "Parametro vazio" })
+      } else { 
+         var obj = req.body 
+         var id = req.params.id 
+         var resposta = dao.update(obj, parseInt(id)) 
+         if (resposta!=null){
+            res.status(200).send({ status: 200, message: "Produto de ID: "+id+" alterado com sucesso!" })
+         } else {
+            res.status(404).send({ status: 404, message: "Produto não localizado" })
+         }
       }
    }
 })
 
 /* DELETE */
 router.delete('/produtos/:id', function(req, res) {
-   var id = req.params.id
-   var index = dao.remove(parseInt(id))
-   if (index!=null) {
-      res.status(200)
-      res.send("Produto de ID: "+id+" removido!")
-   }
-   else {
-      res.status(404)
-      res.send("Produto não localizado")
+   if (!isLogin){
+      res.status(200).send({ status: 200, message: "Necessário efetuar login para acessar este recurso!"})
+   } else {     
+      var id = req.params.id
+      var index = dao.remove(parseInt(id))
+      if (index!=null) {
+         res.status(200).send({ status: 201, message: "Produto de ID: "+id+" removido!"})
+      } else {
+         res.status(404).send({ status: 404, message: "Produto não localizado"})
+      }
    }
 })
 
-//############################################################# LOGIN
+//########################################################################################## LOGIN
 router.post('/login', function(req, res) { 
    console.log('login...')   
    if (req.body.user ==='aluno' && req.body.pass ==='ifsul'){
@@ -90,8 +92,8 @@ router.post('/login', function(req, res) {
          id: 1
       }
       var token = jwt.sign(payload, SECRET, { expiresIn: '5m'})
-
-      res.status(200).send({ token: token})
+      res.status(200).send({ auth: true, token: token})
+      isLogin = true
       console.log('usuario logado...')
    } else {
       res.status(401).send({ user: 'user', pass: 'pass'})
@@ -116,5 +118,10 @@ function verificaToken(req, res, next) {
      next();
    })
 }
+
+router.get('/logout', function(req, res) {
+   isLogin = false
+   res.status(200).send({ auth: false, token: null });
+});
 
 module.exports = router
